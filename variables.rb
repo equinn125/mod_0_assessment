bb2abac4c6da172146160aabc1a6e84f0f00fda3
Erin Quinn <equinn125@gmail.com>
2021-05-20 18:25:15 -0600

    Annotate program specific challenge

--- variables.rb
+++ variables.rb
@@ -1,9 +1,17 @@
+#assign string variable
 animal = "Dog"
+#assign boolean variable
 is_cuddly = true
+#reassign string variable
 animal = "Cat"
+#assign float variable
 num_8 = 7
 num_3 = 21
+#assign variable for quotient
 quotient = num_3 / num_8
+#create array variable
 favorite_films = ["Forrest Gump", "Coco", "Oceans 8"]
+#use shift to remove first element in array
 what_is_shift = favorite_films.shift()
+#use length to print amount of characters in string
 what_is_length = animal.length
