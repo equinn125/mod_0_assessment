66692b7bcaa2106170292c2c0b945f883f6b6689
Erin Quinn <equinn125@gmail.com>
2021-05-20 18:38:32 -0600

    Add models directory

---
+++ models/model_a.rb
@@ -0,0 +1,11 @@
+#create hash for laptop data
+laptop = {
+"brand" => 'Apple',
+"color" => 'Silver',
+"weight_in_pounds" => 5,
+"weight_in_ounces" => 4.5,
+"is_fast" => true,
+"is_old" => false,
+"computer_programs" => ['atom', 'slack', 'spotify'],
+"computer_users" => ['me', 'myself', 'I']
+}
