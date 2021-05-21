0f7cc01fb631df43155f7492b21cbbcd3bb9968f
Erin Quinn <equinn125@gmail.com>
2021-05-20 18:44:23 -0600

    Update model b

---
+++ models/model_b.rb
@@ -0,0 +1,10 @@
+house = {
+"house_color" => 'white',
+"house_location" => "Denver",
+"number_of_rooms" => 4,
+"number_of_bathrooms" => 2.5,
+"is_nice" => true,
+"is_small" => false,
+"names_of_occupants" => ['Bob', 'Deb', 'Sam'],
+"floor_names" => ['Basement', 'Main', 'Upstais']
+}
